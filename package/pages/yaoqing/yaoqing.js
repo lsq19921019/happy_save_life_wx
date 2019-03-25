fbbf5d815ef138dee2279f78a133dffff657b408
liusiqi19921019 <liusiqi19921019@hotmail.com>
2019-03-25 14:32:18 +0800

    提交
    邀请好友页面banner样式调整。一键下载提示语调整。

--- package/pages/yaoqing/yaoqing.js
+++ package/pages/yaoqing/yaoqing.js
@@ -21,6 +21,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    wx.setNavigationBarTitle({
+      title: '邀请好友',
+    })
     console.log('haha');
     let that = this;
 
@@ -76,7 +79,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    wx.setNavigationBarTitle({
+      title: '邀请好友',
+    })
   },
 
   /**
@@ -136,12 +141,32 @@ Page({
           wx.saveImageToPhotosAlbum({
             filePath: res.tempFilePath,
             success: function() {
+              // wx.showToast({
+              //   // title: '分享图已全部保存',
+              //   title: '已保存到相册，快分享给好友吧',
+              //   icon: "success",
+              //   duration: 3000
+              // })
+              // wx.showModal({
+              //   title: '提示',
+              //   content: '已保存到相册，快分享给好友吧',
+              //   success: (showRes) => {
+              //     if (!showRes.confirm) {
+              //       return;
+              //     }
+              //     wx.openSetting({
+              //       success: () => {
+
+              //       }
+              //     })
+              //   }
+              // })
               wx.showToast({
-                title: '分享图已全部保存',
-                icon: "success",
+                title: '已保存到相册，快分享给好友吧',
+                mask: "true",
+                icon: 'none',
                 duration: 3000
               })
-
             },
 
             fail: function(fail) {
